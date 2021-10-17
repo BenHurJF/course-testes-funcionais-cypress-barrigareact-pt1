3de099f929535d22bfcd5dddf35185065b626754
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-10-17 04:14:20 -0300

    add dev dependency xpath e Add login test case

--- cypress/integration/Cenário1.spec.js
+++ cypress/integration/Cenário1.spec.js
@@ -1,3 +1,14 @@
 /// <reference types="Cypress" />
 
-describe('')
+describe('Scenarios tests at a functional level', () => {
+    before('Realizar Login', () => {
+        cy.visit('https://barrigareact.wcaquino.me')
+        cy.get('div[class="input-group"] input').type(Cypress.env('user_email'))
+        cy.get('div[class="form-group"] input[type="password"]').type(Cypress.env('user_senha'), { log: false })
+        
+    })
+
+    it('1- Inserir Conta', () => {
+        
+    })
+})
