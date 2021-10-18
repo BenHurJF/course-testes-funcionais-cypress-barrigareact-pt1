92808dfb63bb53a28657f5101e81a6b6bd5c2c19
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-10-17 22:04:15 -0300

    adição de arquivos que se perderam: pasta plugins

--- cypress/integration/CenáriosTestes.spec.js
+++ cypress/integration/CenáriosTestes.spec.js
@@ -1,12 +1,12 @@
 /// <reference types="Cypress" />
-
+import loc from '../support/locators';
 beforeEach(() => {
         cy.log('Pré condição: Login')
         cy.visit('/')
-        cy.get('div[class="input-group"] input').type(Cypress.env('user_email'))
-        cy.get('div[class="form-group"] input[type="password"]').type(Cypress.env('user_senha'), { log: false })
-        cy.get("button[type='submit']").eq(0).click()
-        cy.get('.toast-message', {timeout:22000}).should('text', 'Bem vindo, Ben-Hur Jeffer!')
+        cy.get(loc.LOGIN.user).type(Cypress.env('user_email'))
+        cy.get(loc.LOGIN.pass).type(Cypress.env('user_senha'), { log: false })
+        cy.get(loc.LOGIN.btnLogin).eq(0).click()
+        cy.get(loc.LOGIN.MSG, {timeout:22000}).should('text', 'Bem vindo, Ben-Hur Jeffer!')
 })
 describe('Cenários de teste', () => {
     it('1- Inserindo Conta', () => {
@@ -19,9 +19,9 @@ describe('Cenários de teste', () => {
     it('2 - Alterando a conta', () => {
         cy.get(`a[class='nav-link dropdown-toggle']`).click()
         cy.get('[href="/contas"]').click()
-        cy.xpath(`//table//td[contains(.,'create account')]/..//i[@class='far fa-edit']`)
-        .click()
-        cy.get('input[data-test="nome"]').type('create account 123')
+        cy.xpath(`//table//td[contains(.,'create account')]/..//i[@class='far fa-edit']`).click()
+        cy.get('input[data-test="nome"]')
+        .clear().type('create account 123')
         cy.get('.btn').click()
         cy.get('.toast-message', {timeout:22000}).should('contain', 'Conta atualizada com sucesso!')
     })
---
+++ cypress/support/locators.js
@@ -0,0 +1,11 @@
+const locators = {
+    LOGIN: {
+        user: 'div[class="input-group"] input',
+        pass: 'div[class="form-group"] input[type="password"]',
+        btnLogin: 'button[type="submit"]',
+        MSG: '.toast-message'
+    }
+
+}
+
+export default  locators;
